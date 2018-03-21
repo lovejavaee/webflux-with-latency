7a0a245e5191a24604a25d19dfc3dad54a6277bb
alex <lovejavaee@126.com>
2018-03-21 23:49:35 +0800

    initial commit

---
+++ src/main/java/com/example/webfluxwithlatency/WebfluxWithLatencyApplication.java
@@ -0,0 +1,12 @@
+package com.example.webfluxwithlatency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxWithLatencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxWithLatencyApplication.class, args);
+	}
+}
